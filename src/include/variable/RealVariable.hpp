71948ea31c965dcda46c1c2241e28dc2390d4e5c
Thibaud Ecarot <akrogames@msn.com>
2016-10-26 17:49:39 +0200

    Change in Readme

--- src/include/variable/RealVariable.hpp
+++ src/include/variable/RealVariable.hpp
@@ -5,19 +5,17 @@
 #ifndef KRATOS_REALVARIABLE_HPP
 #define KRATOS_REALVARIABLE_HPP
 
+#include "core/Variable.hpp"
 
 class RealVariable: public Variable {
 public:
-    RealVariable()  = default;
+    RealVariable() = default; // Default constructor
     RealVariable(RealVariable const &) = default; // Copy constructor
     RealVariable(RealVariable&&) = default; // Move constructor
     RealVariable& operator=(RealVariable const &) = default; // Copy assignment operator
     RealVariable& operator=(RealVariable&&) = default; // Move assignment operator
     ~RealVariable() = default; // Destructor
 
-
-
-private:
 
 };
 
--- src/source/core/Variable.cpp
+++ src/source/core/Variable.cpp
@@ -2,4 +2,4 @@
 // Created by akrogames on 26/10/16.
 //
 
-#include "Variable.hpp"
+#include "core/Variable.hpp"
